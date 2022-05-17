11dec12a5ffe7eb21a556829bab2cd1718c8ad34
andri dwi utomo <andri_dwiutomo@ymail.com>
2022-05-17 08:41:41 +0800

    Custom validation error

---
+++ error.go
@@ -0,0 +1,37 @@
+package responseapi
+
+import (
+	"errors"
+
+	"github.com/go-playground/validator"
+)
+
+type ResponseError interface {
+	GetError(err error) []ApiError
+}
+
+type ApiError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func MsgForTag(tag string) string {
+	switch tag {
+	case "required":
+		return "This field is required"
+	case "email":
+		return "Invalid email"
+	}
+	return ""
+}
+
+func GetError(err error) []ApiError {
+	var ve validator.ValidationErrors
+	out := make([]ApiError, len(ve))
+	if errors.As(err, &ve) {
+		for i, fe := range ve {
+			out[i] = ApiError{Field: fe.Field(), Message: MsgForTag(fe.Tag())}
+		}
+	}
+	return out
+}
