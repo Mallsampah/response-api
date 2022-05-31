ae66b9ec6da2230617a4c8876a91e3c41ba50d73
andri dwi utomo <andri_dwiutomo@ymail.com>
2022-05-31 12:37:07 +0800

    code E002

--- code.go
+++ code.go
@@ -2,12 +2,15 @@ package responseapi
 
 const (
 	InvalidInput = "E001"
+	AlreadyExist = "E002"
 )
 
 func Title(code string) string {
 	switch code {
 	case "E001":
 		return "Invalid Input"
+	case "E002":
+		return "Already Exist"
 	}
 
 	return ""
