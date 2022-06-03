0d34fd95ae49a331ce0b4c3bce6a77929a09372a
andri dwi utomo <andri_dwiutomo@ymail.com>
2022-06-03 19:11:42 +0800

    update code for not exists

--- code.go
+++ code.go
@@ -3,6 +3,7 @@ package responseapi
 const (
 	InvalidInput = "E001"
 	AlreadyExist = "E002"
+	NotExist     = "E003"
 
 	Unauthorized = "A001"
 )
@@ -13,6 +14,8 @@ func Title(code string) string {
 		return "Invalid Input"
 	case "E002":
 		return "Already Exists"
+	case "E003":
+		return "Data Not Exists"
 	case "A001":
 		return "Unauthorized"
 	}
